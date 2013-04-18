68c89a2e86a1fa887a6fd2d0488166121021d4f0
Matthew Lee <mdlthree@gmail.com>
2013-04-18 10:48:22 -0500

    added inverse example

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,8 @@ from flask import Flask, json
 app = Flask(__name__)
 
 import datetime
+import numpy as np
+from numpy import linalg as LA
 
 @app.route('/')
 def index():
@@ -14,8 +16,14 @@ def hello():
 @app.route('/user/<username>')
 def show_user(username):
     date = str(datetime.datetime.now())
-    tmp = username+date
     return json.JSONEncoder().encode([date,username])
+
+@app.route('/inverse/<input>')
+def show_inverse(input):
+    mInit = [int(input[i]) for i in range(len(input))]
+    matrix = np.matrix(mInit).reshape((3,3))
+
+    return '<pre>' + str(LA.inv(matrix)) + '<pre>'
 
 if __name__ == '__main__':
     app.run(debug=True)
