aafb6b884478a5fcecd2998a4ff1f65e0ff4d46d
Matthew Lee <mdlthree@gmail.com>
2013-05-01 15:08:26 -0500

    working case

--- webMining.py
+++ webMining.py
@@ -12,23 +12,15 @@ import matplotlib.pyplot as plt
 import networkx as nx
 # graph and plotting modules
 
-G=nx.Graph()
-# create graph
-
-# <codecell>
-
-root = 'http://www.the-adam.com/'
-absolute_path = set([root])
-stack = [root]
 
 # <codecell>
 
 def findLinks(parent):
+    s = parent.lower()
     try:
         parsed = parse(urlopen(parent))
     except:
-        print 'dead link @: '
-        return
+        return None
 
     newLinks = []
     
@@ -38,56 +30,72 @@ def findLinks(parent):
         if type(t) == str:
             newLinks.append(t)
     
-    s = parent
-    
-    cleanLinks = [i for i in newLinks if not re.match(r"http",i)]
-    abs_path = [s+i for i in cleanLinks if re.match(r"\w", i)]
-    abs_path.extend([s+j[3:] for j in cleanLinks if re.match(r"\W",j)])
-    
+    cleanLinks = [i for i in newLinks if not re.match(r"http",i) ]
+    cleanLinks = [i for i in cleanLinks if not re.match(r"../",i) ]
+    cleanLinks = [i for i in cleanLinks if not re.match(r"mailto",i) ]
+    cleanLinks = [i for i in cleanLinks if not re.match(r"#",i) ]
+    v = len(s) - len(re.split(r"/",s)[-1])
+    abs_path = [ s[:v]+i for i in cleanLinks ] 
     return set(abs_path)  
 
 # <codecell>
 
+root = 'http://www.the-adam.com/'
+absolute_path = set()
+stack = [root]
+
+G=nx.Graph()
+# create graph
+
 stack
 
 # <codecell>
 
-t = findLinks(stack.pop())
-stack.extend(t)
-absolute_path = absolute_path.union(t)
-stack
+pop = stack.pop()
+
+if pop not in absolute_path:
+    absolute_path.add(pop)
+    G.add_node(pop)
+    t = findLinks(pop)
+    if t is not None:
+        stack.extend(t)
+        
+        for v in t:
+            G.add_node(v)
+            G.add_edge(pop, v)
+
+#print len(stack), len(absolute_path)
+
+nx.draw(G, with_labels=False)
+plt.show() # display
 
 # <codecell>
 
-t = findLinks(stack.pop())
-if t:
-    stack.extend(t)
-    absolute_path = absolute_path.union(t)
-    stack
+while len(stack) > 0:
+    pop = stack.pop()
+    
+    if pop not in absolute_path:
+        absolute_path.add(pop)
+        G.add_node(pop)
+        t = findLinks(pop)
+        if t is not None:
+            stack.extend(t)
+            
+            for v in t:
+                G.add_node(v)
+                G.add_edge(pop, v)
+    
+    print len(stack), len(absolute_path)
 
 # <codecell>
 
-t = findLinks(stack.pop())
-if t:
-    stack.extend(t)
-    absolute_path = absolute_path.union(t)
-    stack
+len(absolute_path)
 
 # <codecell>
 
-t = findLinks(stack.pop())
-if t:
-    stack.extend(t)
-    absolute_path = absolute_path.union(t)
-    stack
-
-# <codecell>
-
-t = findLinks(stack.pop())
-if t:
-    stack.extend(t)
-    absolute_path = absolute_path.union(t)
-    stack, absolute_path
+nx.draw(G, with_labels=False, node_size=50)
+plt.figure(figsize=(15,12))
+plt.show() # display
 
 # <codecell>
 
